38ab4ac2f21b7861ffca9045ebba97da798d990b
Bajkus <161233840+Bajkus@users.noreply.github.com>
2025-10-25 14:10:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Login
 client.login(process.env.TOKEN)
-.then(() => console.log(chalk.green(`✅ Bot logged in as ${client.user.tag}`)))
-.catch(err => console.log(chalk.red('❌ Discord login error:'), err));
+.then(() => console.log(`✅ Bot logged in as ${client.user.tag}`)))
+.catch(err => console.log('❌ Discord login error:'), err));
