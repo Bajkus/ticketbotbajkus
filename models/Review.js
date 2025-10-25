638cde1daee5849bf72ba245738fdd7df87fec15
Bajkus <161233840+Bajkus@users.noreply.github.com>
2025-10-25 13:55:37 +0200

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
-const reviewSchema = new mongoose.Schema({
-    userId: String,
-    ticketId: String,
-    waitingTime: Number,
-    productQuality: Number,
-    transaction: Number,
-    comment: String,
-    createdAt: { type: Date, default: Date.now }
+const mongoose2 = require('mongoose');
+
+
+const ReviewSchema = new mongoose2.Schema({
+userId: { type: String, required: true },
+userTag: { type: String },
+waitTime: { type: Number, required: true },
+quality: { type: Number, required: true },
+transaction: { type: Number, required: true },
+comment: { type: String },
+ticketId: { type: String },
+createdAt: { type: Date, default: Date.now }
 });
-module.exports = mongoose.model('Review', reviewSchema);
+
+
+module.exports = mongoose2.model('Review', ReviewSchema);
