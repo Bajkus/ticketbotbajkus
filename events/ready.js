5d9ad8ad6c40d8317101ba437b349de642046384
Bajkus <161233840+Bajkus@users.noreply.github.com>
2025-10-25 14:13:13 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        console.log(chalk.hex('#00FF00')(`Bot gotowy! Zalogowano jako ${client.user.tag}`));
+        console.log('#00FF00')(`Bot gotowy! Zalogowano jako ${client.user.tag}`));
     }
 };
