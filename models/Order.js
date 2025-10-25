63959e322e604c42ccf8daa3ad94190038ee74c1
Bajkus <161233840+Bajkus@users.noreply.github.com>
2025-10-25 13:55:21 +0200

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
-const orderSchema = new mongoose.Schema({
-    userId: String,
-    product: String,
-    quantity: Number,
-    paymentMethod: String,
-    createdAt: { type: Date, default: Date.now }
+
+
+const OrderSchema = new mongoose.Schema({
+userId: { type: String, required: true },
+userTag: { type: String },
+type: { type: String, enum: ['zamowienie', 'reklamacja'], required: true },
+product: { type: String },
+quantity: { type: String },
+payment: { type: String },
+description: { type: String },
+createdAt: { type: Date, default: Date.now }
 });
-module.exports = mongoose.model('Order', orderSchema);
+
+
+module.exports = mongoose.model('Order', OrderSchema);
