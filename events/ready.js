88decf26e693fcaedb2ebc12cf41f0b2fd46112a
Bajkus <161233840+Bajkus@users.noreply.github.com>
2025-10-25 14:04:43 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,9 @@
 const chalk = require('chalk');
+
 module.exports = {
-name: 'ready',
-once: true,
-execute(client) {
-console.log(chalk.green(`Zalogowano jako ${client.user.tag}`));
-}
+    name: 'ready',
+    once: true,
+    execute(client) {
+        console.log(chalk.hex('#00FF00')(`Bot gotowy! Zalogowano jako ${client.user.tag}`));
+    }
 };
